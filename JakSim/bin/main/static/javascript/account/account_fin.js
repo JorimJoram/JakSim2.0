3382bd1195fa55cc7962efd38af899ba79fe52e2
JorimJoram <wkdgyfla97@gmail.com>
2023-08-14 15:09:14 +0900

    backup - restart review

---
+++ JakSim/bin/main/static/javascript/account/account_fin.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    axiosAction();
+}
+
+function axiosAction(){
+    axios.post('/account/register', createJSON())
+        .then(response => {
+            sessionStorage.clear();
+            localStorage.clear();
+            alert('회원가입이 완료되었습니다.');
+            window.location.href='/login';
+        })
+        .catch(error => {
+            console.error(error);
+        })
+}
+
+function createJSON(){
+    return {
+        id : sessionStorage.getItem('id'),
+        pw : sessionStorage.getItem('pw'),
+        name: sessionStorage.getItem('name'),
+        birth: sessionStorage.getItem('birth'),
+        gender: sessionStorage.getItem('gender'),
+        tel : sessionStorage.getItem('tel'),
+        email : sessionStorage.getItem('email')
+    };
+}
